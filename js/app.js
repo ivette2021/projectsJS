76e7a6235c147ab26e323cae128ae244f591e241
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-16 01:17:24 -0300

    implementacion de eslint

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
+/**
+ ===================================================
+ ==esto es un comentario en bloque igual a kotlin===
+ =================================================== 
+ //comentario de una linea
+ *  */
 const nombre = prompt('Cual es tu nombre?') //esto es para que rellenes campos
 document.querySelector('.contenido').innerHTML = `${nombre} esta visitando la javaScript`; //permite seleccionar elementos de html le agregamos un template string
+
+function hola(){
+    console.log('ok');
+    console.log('cancel');
+    console.log('ok');
+    console.log('ok testCommit');
+}

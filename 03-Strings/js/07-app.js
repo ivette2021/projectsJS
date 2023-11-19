28bde37ba060ce4a52487870b283ec8d5ae9a52c
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-19 00:25:59 -0300

    String Methods - Convertir a Mayusculas o Minusculas

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,13 @@
+const producto = ' Monitor 20 Pulgadas';
+console.log(producto.toUpperCase());//cambia todo a mayusculas
+
+
+console.log(producto.toLowerCase());//cambia todo a minusculas
+
+const email = "CORREO@CORREO.COM";//esto es para que no nos tome como que el correo escrito en mayusculas es distinto al de minusculas
+
+console.log(email.toLowerCase());
+
+const precio = 300;
+console.log(precio);//aparece en consola en azul para indicar numero
+console.log(precio.toString());//convertir el int en string (color negro)

2d2a8a10ca7c67d70b6a072f3bd8230c65a55ad1
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-20 11:54:20 -0300

    Boolean JS
    Crear y Comparar Booleans
    Buenas practicas a la hora de evaluar un Boolean

---
+++ 06-Booleans/js/02-app.js
@@ -0,0 +1,7 @@
+const boolean1 = true;
+const boolean2 = false;
+
+
+console.log(boolean1 === boolean2); //false
+console.log(boolean1 === true);//true
+console.log(boolean1 === "true");//false , aqui no es igual a lon que pasa con numeros o string (que igual te lo dejaria pasar)
---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,11 @@
+const autenticado = true;
+
+// if(autenticado){ //no es necesario colocarlo if x==true ya que es implicito por la definicion de variable autentificado
+//     console.log('Si puedes ver netflix');
+// }else{
+//     console.log('No puedes verlo');
+// }  //dar si esta autenticado
+
+//operador ternario
+
+console.log( autenticado ? 'Si esta auntentificado': 'No esta autenticado');//dar si esta autenticado

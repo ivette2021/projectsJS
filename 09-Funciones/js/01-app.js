c74377d677997e5137a9a80a8526af49d5cb1d83
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-23 09:11:29 -0300

    crear funciones en js
    function expression
    function declaration

---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,16 @@
+// declarcion de Funcion (Function Declaration)
+//recuenrda no empezar nombre con numeros 99dias sino dias99
+
+
+function sumar(){ //declarando la funcion
+    console.log (2+2);//no imprime nada
+}
+sumar();//imprime 4
+sumar();//imprime 4
+sumar();//imprime 4
+
+//Expresion de Funcio(Fuction Expression)  2° forma
+const sumar2 = function(){ //nuestra variable fuera igual a funcion
+    console.log(3 +3);
+};
+sumar2();

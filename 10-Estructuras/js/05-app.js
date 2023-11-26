f313112e1f06e8bcc8a08f34ba8678e519980d02
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-25 23:55:08 -0300

    switch

---
+++ 10-Estructuras/js/05-app.js
@@ -0,0 +1,22 @@
+//switch case
+
+const metodoPago = 'efectivo';
+
+switch(metodoPago){
+    case 'efectivo':
+    console.log('Pagaste con ${metodoPago}');
+    break;
+    case 'cheque':
+    console.log('Pagaste con ${metodoPago}');
+    break;
+    case 'tarjeta':
+    console.log('Pagaste con ${metodoPago}');
+    break;
+    default:
+        console.log('Aun no has seleccionado un metodo de pago o metodo de pago no soportado');
+    break;
+}
+
+function pagar(){
+    console.log('Pagando...');
+}

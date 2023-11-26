4c5c9a986fca6eafb38f4a6cb20924ce0d007ee3
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-25 23:24:28 -0300

    estructura de control
    if y comparador estricto

---
+++ 10-Estructuras/js/01-app.js
@@ -0,0 +1,17 @@
+const puntauje = 1000;
+
+//forma 1
+if(puntaje == 1000)
+console.log('si es igual...');
+else
+console.log('no es igual');
+
+
+//forma 2 usando las {}
+if(puntaje == 1000){
+
+console.log('si es igual...');
+
+} else{
+console.log('no es igual');
+}
---
+++ 10-Estructuras/js/02-app.js
@@ -0,0 +1,13 @@
+const puntaje = 1000;
+
+if(puntaje == 1000){ //== son para comparar
+
+    console.log('si es igual...');
+    
+    } else{
+    console.log('no es igual');
+    }
+
+    if(puntaje != 1000){
+        console.log('es diferente...');
+    }

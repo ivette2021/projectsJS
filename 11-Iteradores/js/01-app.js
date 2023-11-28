d1a16a1993c40ddcb8b63e23f3718b15cbb644df
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-27 23:58:43 -0300

    for loop

---
+++ 11-Iteradores/js/01-app.js
@@ -0,0 +1,12 @@
+// for (let i = 0; i < 10; i++) { //i++ lo colocamos i+=2 se incrementa en 2
+//     console.log(`Numero:${i}`);
+    
+// }
+
+// en un for loop como identificas los valores que son pares y lo que son nones
+
+for (let i = 1; i <= 20; i++) {
+    if(i % 2 === 0){ 
+        console.log(`Numero:${i}`);
+    }
+ }

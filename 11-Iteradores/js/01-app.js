42a8fc775bb699689a2e77ac61f258af50f0a79a
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-11-28 00:17:01 -0300

    for loop carrito

--- 11-Iteradores/js/01-app.js
+++ 11-Iteradores/js/01-app.js
@@ -7,6 +7,22 @@
 
 for (let i = 1; i <= 20; i++) {
     if(i % 2 === 0){ 
-        console.log(`Numero:${i}`);
+        console.log(`Numero:${i} es PAR`);//todos los numeros que dejan residuo cero es par
+    }else{
+        console.log(`El numero ${i} es IMPAR`);
     }
+ }
+
+ const carrito = [
+    {nommbre:'Monitor 27 pulgadas', precio: 500},
+    { nombre: 'Television', precio:100},
+    {nombre: 'Tablet', precio: 200},
+    {nombre:'Audifonos', precio: 300},
+    {nombre: ' Celular', precio: 700},
+ ];
+
+ console.log(carrito.length);
+
+ for (let i = 0; i< carrito.length; i++ ) {
+    console.log(carrito[i].nombre);
  }

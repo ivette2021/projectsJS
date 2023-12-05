c1e5cd6f77a669d16786d3881656a29d0934a7fb
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-12-04 23:55:09 -0300

    .some

---
+++ 12-ArrayMethods/js/01-app.js
@@ -0,0 +1,31 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+]
+
+//Comprobar si un valor existe en un arreglo
+
+meses.forEach(mes=> {
+   if (mes == 'Enero') {
+     console.log('Enero si existe');
+   }
+})
+
+//En el mundo real deberias array method
+
+const resultado = meses.includes('Diciembre');
+console.log(resultado);
+
+//EN UN ARREGLO DE OBJETOS SE UTILIZA .some
+const existe = carrito.some(producto => producto.nombre=== 'Monitor Curvo');
+console.log(existe);
+
+//en un arreglo tradicional con .some
+const existe2 = carrito.some(mes => mes === 'Febrero');
+console.log(existe2);

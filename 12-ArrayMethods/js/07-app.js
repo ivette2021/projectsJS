09b757d80739d0c6cb7dcb380a717d8289ca85a2
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-12-05 21:51:06 -0300

    contact

---
+++ 12-ArrayMethods/js/07-app.js
@@ -0,0 +1,13 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', ' Mayo', 'Junio', 'Julio'];
+const meses2 = ['Agosto','Septiembre'];
+const meses3 = ['Octubre','Noviembre','Diciembre'];
+
+//.concat
+const resultado = meses.concat(meses2, meses3);
+
+console.log(resultado);
+
+//spread operator este se utiliza bastante
+const resultado2 = [...meses, ...meses2, ...meses3];
+
+console.log(resultado2);

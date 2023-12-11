c1042e33aa8235125b8abef68ff27e91431de449
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-12-11 09:26:34 -0300

    querySelectorAll

---
+++ 13-DOM/js/05-scripts.js
@@ -0,0 +1,7 @@
+//retornar todos los elementos (nodos)
+
+const card = document.querySelectorAll('.card');
+console.log(card);
+
+const formularios = document.querySelectorAll('#formulario');
+console.log(formularios);

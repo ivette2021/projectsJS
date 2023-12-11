e2e40ae14eaf1144f68f02e25466278b5af23168
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-12-10 23:31:29 -0300

    getElementById

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,5 @@
+//es una buena practica usar los id , pero si hay dos id iguales  te va a retornar el primero que vea no es tan confiable
+
+const formulario = document.getElementById('formulario');
+console.log(formulario);
+
